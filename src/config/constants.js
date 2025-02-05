41531b2b2dd80226b2a18166a836cf74fe97a0e0
이호용 <your_email@example.com>
2025-02-05 11:49:25 +0900

    22651

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,1 +1,1 @@
-export const API_URL = process.env.NODE_ENV === 'production' ? "" : "http://localhost:8080";
+export const API_URL = process.env.NODE_ENV === 'production' ? "https://port-0-faildserver-m5ubwnjb1dcbc5f5.sel4.cloudtype.app" : "http://localhost:8080";
